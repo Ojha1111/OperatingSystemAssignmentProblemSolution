35ff911858024a848e1c589f3e3153f506804767
Amit Kumar Ojha <32640038+Ojha1111@users.noreply.github.com>
2019-04-03 19:13:41 +0530

    Update osSolution16.c

--- osSolution16.c
+++ osSolution16.c
@@ -1,27 +1,27 @@
-//cnt=as qa counter
 #include<stdio.h> 
  int main() 
   { 
  
-     int cnt,j,r,remain,flag=0; 
-     double time,time_quantum;
-     double wait_time=0,turnaround_time=0,arrivaltime[10],Bursttime[10],rt[10]; 
+     int cnt,j,r,re,flag=0; 
+     double time,TQ;
+     double WT=0,TAT=0,AT[10],BT[10],rt[10]; 
      printf("Enter Total Process:\t "); 
      scanf("%d",&r); 
-     remain=r; 
+     re=r; 
      for(cnt=0;cnt<r;cnt++) 
       { 
-        printf("Enter Arrival Time and Burst Time for  Process Number %d :",cnt+1); 
-        scanf("%d",&arrivaltime[cnt]); 
-        scanf("%d",&Bursttime[cnt]); 
-        rt[cnt]=Bursttime[cnt]; 
+        printf("Enter Arrival Time" ,cnt+1); 
+        scanf("%d",&AT[cnt]); 
+        printf("EnterBurst Time for  Process Number %d :",cnt+1); 
+        scanf("%d",&BT[cnt]); 
+        rt[cnt]=BT[cnt]; 
       } 
         printf("Enter Time Quantum:\t"); 
-        scanf("%d",&time_quantum); 
+        scanf("%d",&TQ); 
         printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n"); 
-    for(time=0,cnt=0;remain!=0;) 
+    for(time=0,cnt=0;re!=0;) 
      { 
-    if(rt[cnt]<=time_quantum && rt[cnt]>0) 
+    if(rt[cnt]<=TQ && rt[cnt]>0) 
     { 
       time+=rt[cnt]; 
       rt[cnt]=0; 
@@ -29,26 +29,28 @@
     } 
     else if(rt[cnt]>0) 
     { 
-      rt[cnt]-=time_quantum; 
-      time+=time_quantum; 
+      rt[cnt]-=TQ; 
+      time+=TQ; 
     } 
     if(rt[cnt]==0 && flag==1) 
     { 
-      remain--; 
-      printf("P[%d]\t|\t%d\t|\t%d\n",cnt+1,time-arrivaltime[cnt],time-arrivaltime[cnt]-Bursttime[cnt]); 
-      wait_time+=time-arrivaltime[cnt]-Bursttime[cnt]; 
-      turnaround_time+=time-arrivaltime[cnt]; 
+      re--; 
+      printf("P[%d]\t|\t%d\t|\t%d\n",cnt+1,time-AT[cnt],time-AT[cnt]-BT[cnt]); 
+      
+      WT+=time-AT[cnt]-BT[cnt]; 
+      TAT+=time-AT[cnt]; 
       flag=0; 
     } 
     if(cnt==r-1) 
       cnt=0; 
-    else if(arrivaltime[cnt+1]<=time) 
+    else if(AT[cnt+1]<=time) 
       cnt++; 
     else 
       cnt=0; 
   } 
-  printf("\nAverage Waiting Time= %f\n",wait_time*1.0/r); 
-  printf("Avg Turnaround Time = %f",turnaround_time*1.0/r); 
+  printf("\nAverage Waiting Time= %f\n",WT*1.0/r); 
+  printf("Avg Turnaround Time = %f",TAT*1.0/r); 
   
   return 0; 
 }
+
