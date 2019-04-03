acdf9cee4f9b3655bcd427c4ae871430d65cc559
Amit Kumar Ojha <32640038+Ojha1111@users.noreply.github.com>
2019-04-03 19:14:44 +0530

    Rename osSolution16.c to osSolutions16.c

---
+++ osSolutions16.c
@@ -0,0 +1,62 @@
+//re=remain
+//time quantum=tq
+//waitnig time=WT
+ // turn around time =TAT  
+ //arrival time=AT  
+ //burst time =BT
+#include<stdio.h> 
+ int main() 
+  { 
+ 
+     int cnt,j,r,re,flag=0; 
+     double time,TQ;
+     double WT=0,TAT=0,AT[10],BT[10],rt[10]; 
+     printf("Enter Total Process:\t "); 
+     scanf("%d",&r); 
+     re=r; 
+     for(cnt=0;cnt<r;cnt++) 
+      { 
+        printf("Enter Arrival Time" ,cnt+1); 
+        scanf("%d",&AT[cnt]); 
+        printf("EnterBurst Time for  Process Number %d :",cnt+1); 
+        scanf("%d",&BT[cnt]); 
+        rt[cnt]=BT[cnt]; 
+      } 
+        printf("Enter Time Quantum:\t"); 
+        scanf("%d",&TQ); 
+        printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n"); 
+    for(time=0,cnt=0;re!=0;) 
+     { 
+    if(rt[cnt]<=TQ && rt[cnt]>0) 
+    { 
+      time+=rt[cnt]; 
+      rt[cnt]=0; 
+      flag=1; 
+    } 
+    else if(rt[cnt]>0) 
+    { 
+      rt[cnt]-=TQ; 
+      time+=TQ; 
+    } 
+    if(rt[cnt]==0 && flag==1) 
+    { 
+      re--; 
+      printf("P[%d]\t|\t%d\t|\t%d\n",cnt+1,time-AT[cnt],time-AT[cnt]-BT[cnt]); 
+      
+      WT+=time-AT[cnt]-BT[cnt]; 
+      TAT+=time-AT[cnt]; 
+      flag=0; 
+    } 
+    if(cnt==r-1) 
+      cnt=0; 
+    else if(AT[cnt+1]<=time) 
+      cnt++; 
+    else 
+      cnt=0; 
+  } 
+  printf("\nAverage Waiting Time= %f\n",WT*1.0/r); 
+  printf("Avg Turnaround Time = %f",TAT*1.0/r); 
+  
+  return 0; 
+}
+
